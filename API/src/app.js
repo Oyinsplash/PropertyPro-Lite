0599dfb90fdf0024b8537100fcdbd803252b48fa
Oyinsplash <alabi.oyinkansola14@outlook.com>
2019-07-03 13:43:06 +0100

    feature(server setup): setup server for API [Finishes #167065538]

---
+++ API/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+// Create express app
+const app = express();
+
+// Default route
+app.get("/api/v1", (req, res) =>{
+    res.status(200).json({status: "success", message: 'Welcome to the PropertyPro-Lite API'});
+});
+
+// Response to route that don't exist
+app.use((req, res, next) => {
+    res.status(404).json({status: "404 Not Found", message: "this endpoint does not exist" });
+  });
+
+// Set server port
+  const port = process.env.PORT;
+
+// Listen for Requests 
+app.listen(port, () => {
+    console.log(`This app has started on port: ${port}`);
+  });
